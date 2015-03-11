68915be4d5aa837c4b7bf68f66c4a34c595663a2
direwolf <francis.adkins907@gmail.com>
2015-03-11 12:14:36 -0400

    Implemented basic searching
     - Moved some portions to separate files (css and html to their own files)
     - index.php now takes a post variable and assigns it to a javascript global if it exists
     - that javascript global is then passed to previewing stages if exists, else wildcard
    
     - See notes.txt for next steps.

--- ui/jqueryFilePreview.php
+++ ui/jqueryFilePreview.php
@@ -1,14 +1,30 @@
 <?php
+//echo "File is: " . $_POST['file'];
+//echo "tag_name is: " . $_POST['tag_name'];
+
 $_POST['file'] = urldecode($_POST['file']);
+if( isset($_POST['tag_name']) ) {
+    $_POST['tag_name'] = urldecode($_POST['tag_name']);
+    echo "Listing matches for tag_name: " . $_POST['tag_name'] . '.';
+} else {
+    echo "Listing matches for all tags.";
+}
+
 
 $base_dir = "/";
-//echo "File is: " . $_POST['file'];
 
 $m = new MongoClient();
 $db = $m->RapidApperception;
 $col = $db->TaggingInfo;
 
-$query = array( 'file_path' => $base_dir . $_POST['file'] );
+$query = array();
+if( !isset($_POST['tag_name']) ) {
+    $query = array( 'file_path' => $base_dir . $_POST['file'] );
+} else {
+    $query = array( 'file_path' => $base_dir . $_POST['file'] ,
+                    'tag_name' => $_POST['tag_name']);
+}
+
 $cursor = $col->find( $query );
 $line_nums = "";
 foreach ( $cursor as $document ){
